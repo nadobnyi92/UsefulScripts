4923ad41835824c0f4c2e729ebbb2dab9e09255e
nadobnyi <nadobny92@gmail.com>
2022-02-27 15:29:42 +0300

    add clang-format and shellcheck support

--- vim_init.sh
+++ vim_init.sh
@@ -11,6 +11,9 @@ call vundle#begin()
 Plugin 'gmarik/Vundle.vim'
 Plugin 'scrooloose/nerdtree'
 Plugin 'https://github.com/tpope/vim-fugitive'
+Plugin 'airblade/vim-gitgutter'
+Plugin 'rhysd/vim-clang-format'
+Plugin 'itspriddle/vim-shellcheck'
 
 \" All of your Plugins must be added before the following line
 call vundle#end()            \" required
@@ -109,6 +112,16 @@ set listchars=eol:¶,tab:>·,trail:~,extends:>,precedes:<,space:·
 set list
 
 colorscheme space-vim-dark
+
+\" map to <Leader>cf in C++ code
+autocmd FileType c,cpp,objc nnoremap <buffer><Leader>cf :<C-u>ClangFormat<CR>
+autocmd FileType c,cpp,objc vnoremap <buffer><Leader>cf :ClangFormat<CR>
+\" if you install vim-operator-user
+autocmd FileType c,cpp,objc map <buffer><Leader>x <Plug>(operator-clang-format)
+\" Toggle auto formatting:
+nmap <Leader>C :ClangFormatAutoToggle<CR>
+
+autocmd FileType c ClangFormatAutoEnable
 
 "
 
