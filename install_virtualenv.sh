5f69431760ff67ecd18bf853bdeec8f645599d01
nadobnyi <nadobnyi@debian-BULLSEYE-live-builder-AMD64>
2022-05-26 05:49:32 +0300

    remove unused scripts

--- install_virtualenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt install python3-pip
-pip3 install virtualenv virtaulenvwrapper
-
-export VIRTUALENVWRAPPER_PYTHON=`which python3`
-source `which virtualenvwrapper.sh`
--- source_line_count.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find . -name '*.[cpp|c|h|hpp|py]' -type f | xargs cat | wc -l
